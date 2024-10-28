9008e1509c3fa3217da51ed049034c54076d88f5
ChristianScarabelli <scachmabol@hotmail.it>
2024-10-28 18:15:33 +0100

    ADD: form generates card

--- app/script.js
+++ app/script.js
@@ -45,13 +45,11 @@ const cardTitle = document.getElementById('card_title')
 const cardText = document.getElementById('card_text')
 const cardInfo = document.getElementById('card_info')
 const cardContainer = document.getElementById('card_container')
+const addMemberForm = document.getElementById('add-member-form')
 
 
-// Utilizzo di un ciclo for per creare e aggiungere le cards
-for (let i = 0; i < teamMembers.length; i++) {
-    const member = teamMembers[i]
-
-    // Creazione delle strutture delle card
+// Funzione per creare una card e aggiungerla al DOM
+function createCard(member) {
     const cardDiv = document.createElement('div')
     cardDiv.className = 'col-12 col-md-6 col-lg-4'
 
@@ -88,7 +86,6 @@ for (let i = 0; i < teamMembers.length; i++) {
     email.textContent = member.email
     email.className = 'text-info'
 
-    // Costruzione della card
     cardBody.appendChild(name)
     cardBody.appendChild(role)
     cardBody.appendChild(email)
@@ -100,3 +97,25 @@ for (let i = 0; i < teamMembers.length; i++) {
     cardDiv.appendChild(card)
     cardContainer.appendChild(cardDiv)
 }
+
+// Creazione delle cards iniziali, con funzione applicata ad ogni i dell'array di oggetti
+for (let i = 0; i < teamMembers.length; i++) {
+    createCard(teamMembers[i])
+}
+
+// Funzione per aggiungere un nuovo membro
+function addMember(event) {
+    event.preventDefault()
+    const newMember = {
+        name: document.getElementById('name').value,
+        role: document.getElementById('role').value,
+        email: document.getElementById('email').value,
+        img: document.getElementById('img').value
+    }
+    teamMembers.push(newMember)  // aggiungo il nuovo membro nell'array di oggetti iniziali
+    createCard(newMember)   // applico la funzione per creare la card per il nuovo membro
+    addMemberForm.reset()
+}
+
+// Evento per l'invio del form
+addMemberForm.addEventListener('submit', addMember)
